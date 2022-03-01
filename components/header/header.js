1e1a3c50661b1f5a1a695d154f860a984255a010
Buns <bunsthedev@gmail.com>
2022-02-28 21:29:34 -0600

    remove: chain block

--- components/header/header.js
+++ components/header/header.js
@@ -327,7 +327,7 @@ function Header(props) {
         {unlockOpen && <Unlock modalOpen={unlockOpen} closeModal={closeUnlock} />}
         <TransactionQueue setQueueLength={ setQueueLength } />
     </div>
-    {chainInvalid ? (
+    {/* {chainInvalid ? (
       <div className={classes.chainInvalidError}>
         <div className={classes.ErrorContent}>
           <WrongNetworkIcon className={ classes.networkIcon } />
@@ -337,7 +337,7 @@ function Header(props) {
           <Button className={classes.switchNetworkBtn} variant="contained" onClick={()=>switchChain()} >Switch to { process.env.NEXT_PUBLIC_CHAINID == '4002' ? 'Fantom Testnet' : 'Fantom Mainnet' }</Button>
         </div>
       </div>
-    ) : null}
+    ) : null} */}
     </div>
   );
 }
